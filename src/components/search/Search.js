1322172f68d6f613c671d00bc58537d6f47ca918
gabrielg9 <gabrielg9@interia.pl>
2019-06-05 11:30:17 +0200

    Search by name

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -7,29 +7,20 @@ const Cars = props => (
         <td>{props.car.price}</td>
         <td>{props.car.type}</td>
         <td>{props.car.availability}</td>
-
     </tr>
 )
 
 class productsRead extends Component{
     constructor(props) {
         super(props);
-        this.state = {cars: []};
-        this.handleChange=this.handleChange.bind(this);
-        //this.handleSubmit=this.handleSubmit.bind(this);
+        this.state = {
+            search:'',
+            cars: []};
     }
 
-    handleChange(e){
-        this.setState({[e.target.name]:e.target.value});
+    updateSearch(e){
+        this.setState({search:e.target.value.substr(0,20)});
     }
-
-   /* handleSubmit(e){
-        e.preventDefault();
-        const car={
-            type:this.state.type
-        }
-
-    }*/
 
     componentDidMount() {
         axios.get('http://localhost:4000/products/')
@@ -41,16 +32,24 @@ class productsRead extends Component{
             })
     }
 
-    carList() {
-        return this.state.cars.map(function(currentCar, i){
+    filteredList(){
+        return this.state.cars.filter(
+            (filt)=>
+                filt.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+        ).map(function(currentCar, i){
             return <Cars car={currentCar} key={i} />;
         })
     }
 
     render() {
+        let filteredProducts = this.props.Cars;
         return (
             <div>
-                <h3>List of products</h3>
+                <input type="text"
+                       value={this.state.search}
+                       onChange={this.updateSearch.bind(this)}
+                       placeholder={'search by name'}/>
+
                 <table className="table table-bordered" style={{ marginTop: 20 }} >
                     <thead>
                     <tr>
@@ -61,14 +60,13 @@ class productsRead extends Component{
                     </tr>
                     </thead>
                     <tbody>
-                    { this.carList() }
+                    {this.filteredList()}
                     </tbody>
                 </table>
             </div>
+
+
         )
     }
-
 }
-
-
 export default productsRead;
